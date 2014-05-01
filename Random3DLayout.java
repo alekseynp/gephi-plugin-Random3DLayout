87dca37b16d74dc9d05192612180727f40fc4149
alekseynp <aleksey.nozdrynplotnicki@gmail.com>
2014-05-01 10:27:20 -0700

    Initial Commit
    My code.

---
+++ Random3DLayout.java
@@ -0,0 +1,156 @@
+/*
+Copyright 2008-2011 Gephi
+Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
+Website : http://www.gephi.org
+
+This file is part of Gephi.
+
+DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+Copyright 2011 Gephi Consortium. All rights reserved.
+
+The contents of this file are subject to the terms of either the GNU
+General Public License Version 3 only ("GPL") or the Common
+Development and Distribution License("CDDL") (collectively, the
+"License"). You may not use this file except in compliance with the
+License. You can obtain a copy of the License at
+http://gephi.org/about/legal/license-notice/
+or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+specific language governing permissions and limitations under the
+License.  When distributing the software, include this License Header
+Notice in each file and include the License files at
+/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+License Header, with the fields enclosed by brackets [] replaced by
+your own identifying information:
+"Portions Copyrighted [year] [name of copyright owner]"
+
+If you wish your version of this file to be governed by only the CDDL
+or only the GPL Version 3, indicate your decision by adding
+"[Contributor] elects to include this software in this distribution
+under the [CDDL or GPL Version 3] license." If you do not indicate a
+single choice of license, a recipient has the option to distribute
+your version of this file under either the CDDL, the GPL Version 3 or
+to extend the choice of license to its licensees as provided above.
+However, if you add GPL Version 3 code and therefore, elected the GPL
+Version 3 license, then the option applies only if the new code is
+made subject to such option by the copyright holder.
+
+Contributor(s):
+
+Portions Copyrighted 2011 Gephi Consortium.
+Portions Copyrighted 2014 NGRAIN Corporation.
+ */
+
+package com.ngrain.Random3DLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.gephi.graph.api.Graph;
+import org.gephi.graph.api.GraphModel;
+import org.gephi.graph.api.Node;
+import org.gephi.layout.spi.Layout;
+import org.gephi.layout.spi.LayoutBuilder;
+import org.gephi.layout.spi.LayoutProperty;
+
+/**
+ * Simple randomized layout in 3D space.
+ * <p>
+ * Each node is given a random position x, y, and z based on an area size.
+ * <p>
+ * This class also defines the properties the user can manipulate: area size.
+ * 
+ * @author Aleksey Nozdryn-Plotnicki
+ */
+
+public class Random3DLayout implements Layout {
+    //Architecture
+    private final LayoutBuilder builder;
+    private GraphModel graphModel;
+    //Flags
+    private boolean executing = false;
+    //Properties
+    private int areaSize;
+
+    public Random3DLayout(Random3DLayoutBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public void resetPropertiesValues() {
+        areaSize = 1000;
+    }
+
+    @Override
+    public void initAlgo() {
+        executing = true;
+    }
+
+    @Override
+    public void goAlgo() {
+        Graph graph = graphModel.getGraphVisible();
+        graph.readLock();
+        int nodeCount = graph.getNodeCount();
+        Node[] nodes = graph.getNodes().toArray();
+
+        java.util.Random rand = new java.util.Random();
+        
+        for(int i = 0; i < nodeCount; i++) {
+            Node node = nodes[i];
+            float x = rand.nextFloat()*areaSize;
+            float y = rand.nextFloat()*areaSize;
+            float z = rand.nextFloat()*areaSize;
+            node.getNodeData().setX(x);
+            node.getNodeData().setY(y);
+            node.getNodeData().setZ(z);
+        }
+        
+        graph.readUnlock();
+    }
+
+    @Override
+    public void endAlgo() {
+        executing = false;
+    }
+
+    @Override
+    public boolean canAlgo() {
+        return executing;
+    }
+
+    @Override
+    public LayoutProperty[] getProperties() {
+        List<LayoutProperty> properties = new ArrayList<LayoutProperty>();
+        final String RANDOM3DLAYOUT = "Random 3D Layout";
+
+        try {
+            properties.add(LayoutProperty.createProperty(
+                    this, Integer.class,
+                    "Area size",
+                    RANDOM3DLAYOUT,
+                    "The area size",
+                    "getAreaSize", "setAreaSize"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return properties.toArray(new LayoutProperty[0]);
+    }
+
+    @Override
+    public LayoutBuilder getBuilder() {
+        return builder;
+    }
+
+    @Override
+    public void setGraphModel(GraphModel gm) {
+        this.graphModel = gm;
+    }
+
+    public Integer getAreaSize() {
+        return areaSize;
+    }
+
+    public void setAreaSize(Integer area) {
+        this.areaSize = area;
+    }
+}
